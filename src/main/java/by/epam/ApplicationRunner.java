029b4c79695f4caa8e28146d5386624fb7ac4b3e
Aliaksandr Niunko <a.niunko@astondevs.ru>
2025-12-08 14:50:15 +0300

    lect.4,5,6,7 IoC container , XML  based configuration , Constructor injection, Factiry method injection

--- src/main/java/by/epam/ApplicationRunner.java
+++ src/main/java/by/epam/ApplicationRunner.java
@@ -3,25 +3,20 @@ package by.epam;
 import by.epam.ioc.Container;
 import by.epam.pool.ConnectionPool;
 import by.epam.repository.CompanyRepository;
-import by.epam.repository.UserRepository;
-import by.epam.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
 
-        var container = new Container();
+        var context = new ClassPathXmlApplicationContext("application.xml");
 
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository();
-//        var userService = new UserService(userRepository,companyRepository);
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
 
-        var connectionPool = container.get(ConnectionPool.class);
-        var userRepository = container.get(UserRepository.class);
-        var companyRepository = container.get(CompanyRepository.class);
-        var userService = container.get(UserService.class);
-
+        System.out.println(context.getBean("driver"));
+        System.out.println(connectionPool);
+        CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
 
     }
 
--- src/main/java/by/epam/repository/CompanyRepository.java
+++ src/main/java/by/epam/repository/CompanyRepository.java
@@ -1,5 +1,16 @@
 package by.epam.repository;
 
+import by.epam.pool.ConnectionPool;
+
 public class CompanyRepository {
 
+    private final ConnectionPool connectionPool;
+
+    public CompanyRepository(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
+
+    public static CompanyRepository of(ConnectionPool pool){
+        return new CompanyRepository(pool);
+    }
 }
