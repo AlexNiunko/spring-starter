179292c16bc635d98bf9ec798513321a73f773d6
Aliaksandr Niunko <a.niunko@astondevs.ru>
2025-12-11 21:07:43 +0300

    lect.13 Custom Bean Factory Post Processor, Ordered

---
+++ src/main/java/by/epam/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,20 @@
+package by.epam.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+}
