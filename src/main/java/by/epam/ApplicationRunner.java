35c00ee57b899198809624cccb968e4d99322ee1
Aliaksandr Niunko <a.niunko@astondevs.ru>
2025-12-11 12:56:08 +0300

    lect.12 Bean Factory Post Processor

--- src/main/java/by/epam/ApplicationRunner.java
+++ src/main/java/by/epam/ApplicationRunner.java
@@ -8,6 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+        String res="er";
+
         try(var context = new ClassPathXmlApplicationContext("application.xml")){
             var connectionPool = context.getBean("p1", ConnectionPool.class);
 
