645854c20fa2bcc72fabeefb41dc8cddd5f092aa
Aliaksandr Niunko <a.niunko@astondevs.ru>
2025-12-13 19:23:28 +0300

    lect.20-21 Bean Definition Readers , Type Filters

--- src/main/java/by/epam/ApplicationRunner.java
+++ src/main/java/by/epam/ApplicationRunner.java
@@ -7,7 +7,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        String res="er";
 
         try(var context = new ClassPathXmlApplicationContext("application.xml")){
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
