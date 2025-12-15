f91662471089119e6bc0c11b385b68c987a62f33
Aliaksandr Niunko <a.niunko@astondevs.ru>
2025-12-15 10:37:38 +0300

    lect.24 Java based configuration

--- src/main/java/by/epam/ApplicationRunner.java
+++ src/main/java/by/epam/ApplicationRunner.java
@@ -1,17 +1,18 @@
 package by.epam;
 
+import by.epam.config.ApplicationConfiguration;
 import by.epam.pool.ConnectionPool;
 import by.epam.repository.CrudRepository;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
 
-        try(var context = new ClassPathXmlApplicationContext("application.xml")){
+        try(var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)){
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
 
-            System.out.println(context.getBean("driver"));
             CrudRepository companyRepository = context.getBean("companyRepository", CrudRepository.class);
             System.out.println(companyRepository.findById(1));
 
