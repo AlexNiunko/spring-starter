8da70268db739268ab2fa58fdf92167578c76756
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-10 22:50:55 +0300

    lect.26-27 @Bean

--- src/main/java/by/epam/spring/config/ApplicationConfiguration.java
+++ src/main/java/by/epam/spring/config/ApplicationConfiguration.java
@@ -1,24 +1,49 @@
 package by.epam.spring.config;
 
+import by.epam.spring.pool.ConnectionPool;
 import by.epam.spring.repository.CrudRepository;
+import by.epam.spring.repository.UserRepository;
 import by.epam.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
 
 //@ImportResource("classpath:application.xml")
 @Import(WebConfiguration.class)
-@Configuration
+@Configuration(proxyBeanMethods = true)
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "by.epam",
-useDefaultFilters = false,
-includeFilters = {
-    @Filter(type = FilterType.ANNOTATION,value = Component.class),
-    @Filter(type = FilterType.ASSIGNABLE_TYPE,value = CrudRepository.class),
-    @Filter(type = FilterType.REGEX,pattern = "by\\..+Repository")
-})
+        useDefaultFilters = false,
+        includeFilters = {
+                @Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+                @Filter(type = FilterType.REGEX, pattern = "by\\..+Repository")
+        })
 public class ApplicationConfiguration {
 
+    @Bean("pool2")
+    public ConnectionPool pool2(@Value("${db.username}") String userName) {
+        return new ConnectionPool(userName, 20);
+    }
+
+    @Bean
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 20);
+    }
+
+    @Bean
+    public UserRepository userRepository(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository2() {
+        var pool1 = pool3();
+        var pool2 = pool3();
+        var pool3 = pool3();
+        return new UserRepository(pool3());
+    }
 
 
 }
