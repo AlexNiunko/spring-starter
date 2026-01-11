9e2951b9b6652816b070cb41931c8f65512c4a2d
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-12 00:18:38 +0300

    lect.39 SpringBootStarterLogger

--- src/main/java/by/epam/spring/database/pool/ConnectionPool.java
+++ src/main/java/by/epam/spring/database/pool/ConnectionPool.java
@@ -4,9 +4,11 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component("pool1")
 @RequiredArgsConstructor
 public class ConnectionPool {
@@ -20,11 +22,11 @@ public class ConnectionPool {
 
     @PostConstruct
     private void init() {
-        System.out.println("Init connection pool");
+        log.info("Init connection pool");
     }
 
     @PreDestroy
     private void destroy() {
-        System.out.println("Clean connection pool");
+        log.info("Clean connection pool");
     }
 }
--- src/main/java/by/epam/spring/database/repository/CompanyRepository.java
+++ src/main/java/by/epam/spring/database/repository/CompanyRepository.java
@@ -5,6 +5,7 @@ import by.epam.spring.bpp.Transaction;
 import by.epam.spring.database.entity.Company;
 import by.epam.spring.database.pool.ConnectionPool;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 @Transaction
 @Auditing
@@ -27,18 +29,18 @@ public class CompanyRepository implements CrudRepository<Integer, Company> {
 
     @PostConstruct
     private void init() {
-        System.out.println("init company repository");
+        log.info("init company repository");
     }
 
     @Override
     public Optional<Company> findById(Integer id) {
-        System.out.println("findById method ... ");
+        log.info("findById method ... ");
         return Optional.of(new Company(id));
     }
 
     @Override
     public void delete(Company entity) {
-        System.out.println("delete method ... ");
+        log.info("delete method ... ");
     }
 
 }
