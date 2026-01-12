2d0d49e1e9ff076b8a87dc519adc16a5cbe5f409
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-12 16:24:28 +0300

    lect.41 TestStarter unit test

---
+++ src/test/java/by/epam/spring/service/CompanyServiceTest.java
@@ -0,0 +1,52 @@
+package by.epam.spring.service;
+
+import by.epam.spring.database.entity.Company;
+import by.epam.spring.database.repository.CrudRepository;
+import by.epam.spring.dto.CompanyReadDto;
+import by.epam.spring.listener.entity.EntityEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceTest {
+
+    public static final Integer COMPANY_ID = 1;
+
+    @Mock
+    private CrudRepository<Integer, Company> companyRepository;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        Mockito.doReturn(Optional.of(new Company(COMPANY_ID)))
+                .when(companyRepository).findById(COMPANY_ID);
+
+        var actualResult = companyService.findById(COMPANY_ID);
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual->assertEquals(expectedResult,actual));
+
+        verify(applicationEventPublisher).publishEvent(any(EntityEvent.class));
+        verifyNoMoreInteractions(applicationEventPublisher,userService);
+
+    }
+}
