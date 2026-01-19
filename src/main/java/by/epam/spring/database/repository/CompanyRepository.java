12ba421c9dd8cb0423dd422aa24b25f100c5ed8f
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-19 13:27:44 +0300

    lect.55 Part Tree Repository Query

--- src/main/java/by/epam/spring/database/repository/CompanyRepository.java
+++ src/main/java/by/epam/spring/database/repository/CompanyRepository.java
@@ -1,14 +1,17 @@
 package by.epam.spring.database.repository;
 
 import by.epam.spring.database.entity.Company;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface CompanyRepository extends Repository<Company,Integer > {
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
-    Optional<Company>findById(Integer id);
+    // Optional , Entity , Future
+    Optional<Company> findByName(String name);
 
-    void delete(Company entity);
+    // Collection,  Stream (batch,close() )
+    List<Company> findAllByNameContainingIgnoreCase(String fragment);
 
 }
