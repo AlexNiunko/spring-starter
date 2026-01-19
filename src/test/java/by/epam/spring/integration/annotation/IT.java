e9afa678db9d1c01c6c9535239c602ee3999fd5d
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-19 09:26:27 +0300

    lect.52 Manual Transactions

--- src/test/java/by/epam/spring/integration/annotation/IT.java
+++ src/test/java/by/epam/spring/integration/annotation/IT.java
@@ -3,6 +3,7 @@ package by.epam.spring.integration.annotation;
 import by.epam.spring.integration.TestApplicationRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,6 +13,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest(classes = TestApplicationRunner.class)
 public @interface IT {
 }
--- src/test/java/by/epam/spring/integration/database/repository/CompanyRepositoryTest.java
+++ src/test/java/by/epam/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
 
@@ -16,18 +17,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @IT
-@Transactional
 //@Commit // или @Commit если
 @RequiredArgsConstructor
 class CompanyRepositoryTest {
 
     private final EntityManager entityManager;
+    private final TransactionTemplate transactionTemplate;
 
     @Test
     void findById() {
-        var company = entityManager.find(Company.class, 1);
-        assertNotNull(company);
-        assertThat(company.getLocales()).hasSize(2);
+        transactionTemplate.executeWithoutResult(tx->{
+            var company = entityManager.find(Company.class, 1);
+            assertNotNull(company);
+            assertThat(company.getLocales()).hasSize(2);
+        });
+
     }
 
     @Test
