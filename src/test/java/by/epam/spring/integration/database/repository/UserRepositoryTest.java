57a2e0aa2041695f1a8297168f240d53beeef567
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-20 21:05:32 +0300

    lect.70 In memory databases H2

--- src/test/java/by/epam/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/by/epam/spring/integration/database/repository/UserRepositoryTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -22,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @IT
+@Sql(scripts = {"classpath:sql/data.sql"})
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
