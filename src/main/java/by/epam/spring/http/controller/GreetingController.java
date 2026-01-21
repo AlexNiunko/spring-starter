591775d81c14b78283dcfa8d736b4a354dd3b53c
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-21 12:08:49 +0300

    lect.77 @RequestMapping

--- src/main/java/by/epam/spring/http/controller/GreetingController.java
+++ src/main/java/by/epam/spring/http/controller/GreetingController.java
@@ -3,17 +3,21 @@ package by.epam.spring.http.controller;
 import by.epam.spring.database.repository.CompanyRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/api/v1")
 public class GreetingController {
 
-
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository){
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
+    @GetMapping("/bye")
     public ModelAndView bye(ModelAndView modelAndView){
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
