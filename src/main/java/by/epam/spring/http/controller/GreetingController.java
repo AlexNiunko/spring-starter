796007c4bb47b8395e1232ac4d466b63389a157a
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-21 13:05:04 +0300

    lect.78 parameters,headers,cookies

--- src/main/java/by/epam/spring/http/controller/GreetingController.java
+++ src/main/java/by/epam/spring/http/controller/GreetingController.java
@@ -1,18 +1,25 @@
 package by.epam.spring.http.controller;
 
 import by.epam.spring.database.repository.CompanyRepository;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/api/v1")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository){
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam("age") Integer age,
+                              @RequestHeader("accept") String accept,
+                              @CookieValue("JSESSIONID") String jsessionID,
+                              @PathVariable("id") Integer id){
+        String ageParamValue=request.getParameter("age");
+        var acceptHeader = request.getHeader("accept");
+        Cookie[] cookies = request.getCookies();
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
