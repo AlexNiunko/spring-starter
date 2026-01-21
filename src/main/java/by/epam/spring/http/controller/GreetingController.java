f5d76c52fc79c869a58aea73a169348a890d0c4b
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-21 18:10:42 +0300

    lect.79 Model

--- src/main/java/by/epam/spring/http/controller/GreetingController.java
+++ src/main/java/by/epam/spring/http/controller/GreetingController.java
@@ -1,32 +1,51 @@
 package by.epam.spring.http.controller;
 
-import by.epam.spring.database.repository.CompanyRepository;
+import by.epam.spring.dto.UserReadDto;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
 
-    @GetMapping("/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
-                              @RequestParam("age") Integer age,
-                              @RequestHeader("accept") String accept,
-                              @CookieValue("JSESSIONID") String jsessionID,
-                              @PathVariable("id") Integer id){
-        String ageParamValue=request.getParameter("age");
-        var acceptHeader = request.getHeader("accept");
-        Cookie[] cookies = request.getCookies();
+    @GetMapping("/hello")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+//        request.getSession().setAttribute();
+//        request.setAttribute(); requestScope
         modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
         return modelAndView;
     }
 
     @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView){
+    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
+        ModelAndView modelAndView=new ModelAndView();
         modelAndView.setViewName("greeting/bye");
+        return modelAndView;
+    }
+
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
+                               @RequestParam("age") Integer age,
+                               @RequestHeader("accept") String accept,
+                               @CookieValue("JSESSIONID") String jsessionID,
+                               @PathVariable("id") Integer id) {
+        String ageParamValue = request.getParameter("age");
+        var acceptHeader = request.getHeader("accept");
+        Cookie[] cookies = request.getCookies();
+        modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
--- src/main/webapp/WEB-INF/jsp/greeting/bye.jsp
+++ src/main/webapp/WEB-INF/jsp/greeting/bye.jsp
@@ -5,6 +5,6 @@
     <title>Bye</title>
 </head>
 <body>
-Bye baby!!
+Bye ${sessionScope.user.username} ${sessionScope.user.id}!!
 </body>
 </html>
