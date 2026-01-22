494830c23d6f1f30370315f53d9913ac6c1fe214
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-22 10:17:51 +0300

    lect.85 Converter and Formatter

--- src/main/java/by/epam/spring/dto/UserCreateEditDto.java
+++ src/main/java/by/epam/spring/dto/UserCreateEditDto.java
@@ -4,6 +4,7 @@ import by.epam.spring.database.entity.Role;
 import java.time.LocalDate;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Value
 @FieldNameConstants
@@ -12,6 +13,7 @@ public class UserCreateEditDto {
     String username;
     String firstname;
     String lastname;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     LocalDate birthDate;
     Role role;
     Integer companyId;
