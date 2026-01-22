0629f9a4074e8743c554df4e8e47a9094a35a6be
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-22 21:42:54 +0300

    lect.91 Validation Starter

--- src/main/java/by/epam/spring/dto/UserCreateEditDto.java
+++ src/main/java/by/epam/spring/dto/UserCreateEditDto.java
@@ -1,6 +1,9 @@
 package by.epam.spring.dto;
 
 import by.epam.spring.database.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
@@ -10,8 +13,14 @@ import org.springframework.format.annotation.DateTimeFormat;
 @FieldNameConstants
 public class UserCreateEditDto {
 
+    @Email
     String username;
+
+    @NotNull
+    @Size(min = 3, max = 64)
     String firstname;
+
+    @NotNull
     String lastname;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     LocalDate birthDate;
