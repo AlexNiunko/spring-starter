6fe2301ec85857566fb65fb429b7167ca527a1ae
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-24 10:00:43 +0300

    lect.103 Http Basic Authentication

--- src/main/java/by/epam/spring/config/SecurityConfiguration.java
+++ src/main/java/by/epam/spring/config/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package by.epam.spring.config;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +16,8 @@ public class SecurityConfiguration {
     @SneakyThrows
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        return http.csrf(csrf -> csrf.disable())
+        return http.csrf(AbstractHttpConfigurer::disable)
+//                .httpBasic(Customizer.withDefaults())
                 .formLogin(form -> form.loginPage("/login")
                         .defaultSuccessUrl("/users")
                         .permitAll())
