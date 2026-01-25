4d97e2dab7f5a54847007cddd5aee997490787aa
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-25 17:41:11 +0300

    lect.111-113 OAuth 2.0 part.2

--- src/main/java/by/epam/spring/config/SecurityConfiguration.java
+++ src/main/java/by/epam/spring/config/SecurityConfiguration.java
@@ -1,6 +1,10 @@
 package by.epam.spring.config;
 
 import by.epam.spring.database.entity.Role;
+import by.epam.spring.service.UserService;
+import java.lang.reflect.Proxy;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,19 +12,46 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private final UserService userService;
+
+
+
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return userRequest -> {
+            String email = userRequest.getIdToken().getClaim("email");
+            //TODO create new User()
+            UserDetails userDetails = userService.loadUserByUsername(email);
+//            new OidcUserService().loadUser()
+            var defaultOidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+
+            var methods = Set.of(UserDetails.class.getMethods());
+
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{UserDetails.class, OidcUser.class}, (proxy, method, args) ->
+                            methods.contains(method)
+                                    ? method.invoke(userDetails, args)
+                                    : method.invoke(defaultOidcUser,args)
+
+            );
+        };
+
     }
+
 
     @SneakyThrows
     @Bean
@@ -38,9 +69,14 @@ public class SecurityConfiguration {
                         .requestMatchers("/users/{id}/delete").hasAuthority("ADMIN")
                         .requestMatchers("/admin/**").hasAuthority(Role.ADMIN.getAuthority())
                         .anyRequest().authenticated()
-
                 )
+                .oauth2Login(config -> config
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .oidcUserService(oidcUserService())))
                 .build();
     }
+
 
 }
