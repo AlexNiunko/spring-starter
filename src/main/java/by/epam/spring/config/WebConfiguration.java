d17dc19de3611eb85f92889d6261db5872390b73
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-25 19:18:47 +0300

    lect.117 i18 ThymeLeaf

--- src/main/java/by/epam/spring/config/WebConfiguration.java
+++ src/main/java/by/epam/spring/config/WebConfiguration.java
@@ -1,15 +1,42 @@
 package by.epam.spring.config;
 
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(localeChangeInterceptor());
+    }
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
 //        registry.addConverter(Jsr310Converters.StringToLocalDateConverter.INSTANCE);
+    }
+
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        var cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.US);
+        return cookieLocaleResolver;
     }
 }
