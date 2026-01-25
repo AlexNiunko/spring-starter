b2ef287067aa21f40e3685aa5c62cf9c270423b3
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-01-26 00:33:43 +0300

    lect.123 @Around advise

--- src/main/java/by/epam/spring/aop/FirstAspect.java
+++ src/main/java/by/epam/spring/aop/FirstAspect.java
@@ -2,9 +2,11 @@ package by.epam.spring.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -100,22 +102,39 @@ public class FirstAspect {
                            Object service,
                            Object serviceProxy,
                            Transactional transactional) {
-        log.info("Invoked findById method in class {},with id {}",service,id);
+        log.info("Invoked findById method in class {},with id {}", service, id);
     }
 
-    @AfterReturning(value = "anyFindByIdServiceMethod() && target(service)",returning = "result", argNames = "result,service")
-    public void addLoggingAfterReturning(Object result,Object service){
-        log.info("after returning - invoked findById method in class {},result {}",service,result);
+    @AfterReturning(value = "anyFindByIdServiceMethod() && target(service)", returning = "result", argNames = "result,service")
+    public void addLoggingAfterReturning(Object result, Object service) {
+        log.info("after returning - invoked findById method in class {},result {}", service, result);
     }
 
-    @AfterThrowing(value = "anyFindByIdServiceMethod() && target(service)",throwing = "exception")
-    public void addLoggingAfterThrowing(Throwable exception,Object service){
-        log.info("after throwing - invoked findById method in class {},exception {}: {}",service,exception.getClass(),exception.getMessage());
+    @AfterThrowing(value = "anyFindByIdServiceMethod() && target(service)", throwing = "exception")
+    public void addLoggingAfterThrowing(Throwable exception, Object service) {
+        log.info("after throwing - invoked findById method in class {},exception {}: {}", service, exception.getClass(), exception.getMessage());
     }
 
     @After(value = "anyFindByIdServiceMethod() && target(service)")
-    public void addLoggingAfterFinally(Object service){
-        log.info("after finally - invoked findById method in class {}",service);
+    public void addLoggingAfterFinally(Object service) {
+        log.info("after finally - invoked findById method in class {}", service);
+    }
+
+    @Around(value = "anyFindByIdServiceMethod() && target(service) && args(id)", argNames = "joinPoint,service,id")
+    public Object addLoggingAround(ProceedingJoinPoint joinPoint, Object service, Object id) throws Throwable {
+        log.info("AROUND Invoked findById method in class {},with id {}", service, id);
+        try {
+            var result = joinPoint.proceed();
+            log.info("AROUND after returning - invoked findById method in class {},result {}", service, result);
+            return result;
+
+        } catch (Throwable exception) {
+            log.info("AROUND after throwing - invoked findById method in class {},exception {}: {}", service, exception.getClass(), exception.getMessage());
+            throw exception;
+        } finally {
+            log.info("AROUND after finally - invoked findById method in class {}", service);
+        }
+
     }
 
 }
